710769540440b01dab69899613bbb3d6f08397c6
Philip Cali <philip.cali@gmail.com>
2012-09-08 10:25:47 -0500

    Rename the files appropriately.

--- src/main/scala/client.scala
+++ src/main/scala/client.scala
@@ -1,101 +1,77 @@
 package com.github.philcali.puppet
+import actions._
 
 import dispatch._
 
 import lmxml.{ LmxmlNode, TextNode, ParsedNode, SinglePass }
 
-import com.ning.http.client.{
-  AsyncHttpClientConfig => Config,
-  AsyncHttpClient,
-  Response
-}
-
 import java.io.{ File, FileOutputStream }
 
-case class PuppetClient(config: Config) extends Executor {
-  val client = new AsyncHttpClient(config)
+object TestMan extends App {
+  object Lmxml extends lmxml.PlainLmxmlFactory with lmxml.FileLoading
 
-  val timeout = Duration.Zero
-}
+  val promises = Lmxml.fromFile("instructions.lmxml")(Instructions.Default)
 
-object PuppetConfig {
-  import collection.JavaConversions.asScalaSet
-
-  val defaultConfig = default.build
-
-  def default = new Config.Builder
-
-  def loadMap(options: Map[String, String], config: Config.Builder = default) = {
-      config.setFollowRedirects(
-        options.get("follow-redirects").map(_.toBoolean)
-               .getOrElse(defaultConfig.isRedirectEnabled)
-      ).setAllowPoolingConnection(
-        options.get("keep-alive").map(_.toBoolean)
-               .getOrElse(defaultConfig.getAllowPoolingConnection)
-      ).setUserAgent(
-        options.get("user-agent").getOrElse(defaultConfig.getUserAgent)
-      ).setMaximumConnectionsTotal(
-        options.get("max-connections").map(_.toInt)
-               .getOrElse(defaultConfig.getMaxTotalConnections)
-      ).setCompressionEnabled(
-        options.get("compression-enabled").map(_.toBoolean)
-               .getOrElse(defaultConfig.isCompressionEnabled)
-      ).setRequestCompressionLevel(
-        options.get("request-compression-level").map(_.toInt)
-               .getOrElse(defaultConfig.getRequestCompressionLevel)
-      ).setIdleConnectionInPoolTimeoutInMs(
-        options.get("idle-connection-timeout").map(_.toInt)
-               .getOrElse(defaultConfig.getIdleConnectionInPoolTimeoutInMs)
-      ).setRequestTimeoutInMs(
-        options.get("request-timeout").map(_.toInt)
-               .getOrElse(defaultConfig.getRequestTimeoutInMs)
-      )
-  }
-
-  def fromFile(loc: String, origin: Config.Builder = default) =
-    loadFile(loc).map(loadMap(_, origin)).getOrElse(origin)
-
-  def fromLmxml(nodes: Seq[ParsedNode], origin: Config.Builder = default) =
-    flattenConfigNodes(nodes).map(loadMap(_, origin)).getOrElse(origin)
-
-  def flattenConfigNodes(nodes: Seq[ParsedNode]) = {
-    val toText = (node: Option[ParsedNode]) =>
-      node.filter(_.isInstanceOf[TextNode])
-          .map(_.asInstanceOf[TextNode])
-          .map(_.contents).getOrElse("")
-
-    nodes.headOption.filter(_.name == "config").map { node =>
-      Map(node.children.foldLeft(List[(String, String)]())({
-        case (in, n) => (n.name, toText(n.children.headOption)) :: in
-      }):_*)
-    }
-  }
-
-  private def loadFile(loc: String) = {
-    val file = new File(loc)
-    if (file.exists) {
-      val in = new java.io.FileInputStream(file)
-      val p = new java.util.Properties()
-      p.load(in)
-      in.close()
-      Some(p.entrySet.map(e => e.getKey.toString -> e.getValue.toString).toMap)
-    } else {
-      None
-    }
+  for {
+    promise <- promises
+    lines <- promise
+  } {
+    lines foreach println
   }
 }
 
-case class Context(data: Map[String, Any]) extends (Response => Context) {
-  def response = get[Response]("response")
+object Instructions {
+  lazy val defaultSet: Map[String, Action] = Map(
+    "go" -> GoAction,
+    "find" -> FindAction,
+    "set" -> SetAction,
+    "each" -> EachAction,
+    "download" -> DownloadAction,
+    "println" -> PrintAction
+  )
 
-  def get[A](key: String) = data.get(key).map(_.asInstanceOf[A])
+  lazy val Default = Instructions(defaultSet)
 
-  def unwrap(kv: (String, Option[_])) =
-    kv._2.map(p => this + (kv._1 -> p)).getOrElse(this)
+  def apply(actions: (String, Action)*) = new Instructions(Map(actions:_*))
+}
 
-  def + (kv: (String, Any)) = Context(data + kv)
+case class Instructions(actions: Map[String, Action]) extends SinglePass[Promise[List[String]]] {
 
-  def apply(r: Response) = this + ("response" -> r)
+  type Nodes = Seq[ParsedNode]
 
-  override def toString() = "Context(data = %s)" format data.toString
+  def single(node: ParsedNode) = node match {
+    case LmxmlNode(name, attrs, children) if name == "browser" =>
+      val config = attrs.get("file")
+                        .map(PuppetConfig.fromFile(_))
+                        .getOrElse(PuppetConfig.default)
+
+      val browser = PuppetClient(PuppetConfig.fromLmxml(children, config).build)
+      instructions(browser, children.tail, Context(Map.empty)).onComplete({
+        case _ => browser.shutdown()
+      })
+  }
+
+  def instructions(cl: PuppetClient, nodes: Nodes, ctx: Context): Promise[List[String]] = nodes match {
+    case node :: ns if node.name == "instructions" =>
+      for {
+        first <- instructions(cl, node.children, ctx)
+        second <- instructions(cl, ns, ctx)
+      } yield (first ::: second)
+    case (node: LmxmlNode) :: ns =>
+      val head = actions.get(node.name) match {
+        case Some(fun) => fun(node, cl, ctx).apply(this)
+        case _ => Promise(Nil)
+      }
+      for {
+        first <- head
+        second <- instructions(cl, ns, ctx)
+      } yield (first ::: second)
+    case lmxml.CommentNode(_) :: ns => instructions(cl, ns, ctx)
+    case node :: ns =>
+      for {
+        first <- instructions(cl, node.children, ctx)
+        second <- instructions(cl, ns, ctx)
+      } yield (first ::: second)
+    case Nil => Promise(Nil)
+  }
 }
